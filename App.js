0b50eef9a469c749ed935bc554f237c667664806
ALLYSON FELIPPO BREDA DE MONTE CHRISTO <allyson_christo@estudante.sesisenai.org.br>
2024-04-10 22:06:47 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,56 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
 
 export default function App() {
+  const [contador, setContador] = React.useState(0)
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={estilos.container}>
+      <TouchableOpacity
+        onPress={() => setContador(contador - 1)}
+        style={estilos.btns}>
+        <Text>
+          -
+        </Text>
+      </TouchableOpacity>
+      <Text style={estilos.contador}>{contador}</Text>
+      <TouchableOpacity
+        onPress={() => setContador(contador + 1)}
+        style={estilos.btns}>
+        <Text>
+          +
+        </Text>
+      </TouchableOpacity>
+
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const estilos = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f7ff',
     alignItems: 'center',
     justifyContent: 'center',
+    flexDirection: 'row',
+    gap: 16
   },
+
+  contador: {
+    fontSize: 72,
+    color: '#fff'
+  },
+
+  btns: {
+    backgroundColor: '#fff',
+    width: 64,
+    height: 64,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 50,
+    fontSize: 64
+  }
 });
